3777e32bad36206d295fef7d1f6bf5ad8ee13e81
palimpZest <polo.vaca365@gmail.com>
2017-12-04 17:00:37 +0100

    initial commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+function clock() {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  minutes = check(minutes);
+  seconds = check(seconds);
+  document.getElementById("demo").innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+let time = setInterval(clock, 500);
+function check(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
